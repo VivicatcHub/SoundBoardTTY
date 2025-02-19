85154a42daaca052b2fc16fe9b3adad336b83b8c
Guinet Valentin <valentin.guinet@epitech.eu>
2025-02-19 16:32:42 +0100

    [c-s] bc gestion

--- src/bd_gestion.c
+++ src/bd_gestion.c
@@ -1,8 +1,28 @@
 #include "header.h"
 
+static create_sounds(FILE *file)
+{
+    char line[512];
+    char *name;
+    char *path;
+
+    while (fgets(line, sizeof(line), file)) {
+        name = strtok(line, ",");
+        path = strtok(NULL, "\n");
+        if (name && path) {
+            strncpy(sounds[sound_count].name, name,
+                sizeof(sounds[sound_count].name));
+            strncpy(sounds[sound_count].path, path,
+                sizeof(sounds[sound_count].path));
+            sound_count++;
+        }
+    }
+}
+
 void read_sounds(void)
 {
     FILE *file = fopen(sounds_file_path, "r");
+
     if (!file) {
         printf("%s\n", sounds_file_path);
         file = fopen(sounds_file_path, "w");
@@ -14,21 +34,8 @@ void read_sounds(void)
         printw("Le fichier .txt a été créé.\n");
         return;
     }
-
-    char line[512];
-    while (fgets(line, sizeof(line), file)) {
-        char *name = strtok(line, ",");
-        char *path = strtok(NULL, "\n");
-        if (name && path) {
-            strncpy(sounds[sound_count].name, name,
-                sizeof(sounds[sound_count].name));
-            strncpy(sounds[sound_count].path, path,
-                sizeof(sounds[sound_count].path));
-            sound_count++;
-        }
-    }
+    create_sounds(file);
     fclose(file);
-
     if (sound_count == 0)
         printw("Il n'y a pas de sons dans la liste.\n");
 }
@@ -36,11 +43,11 @@ void read_sounds(void)
 void write_sounds(void)
 {
     FILE *file = fopen(sounds_file_path, "w");
+
     if (!file) {
         perror("Failed to open .txt for writing");
         exit(EXIT_FAILURE);
     }
-
     for (int i = 0; i < sound_count; i++)
         fprintf(file, "%s,%s\n", sounds[i].name, sounds[i].path);
     fclose(file);
